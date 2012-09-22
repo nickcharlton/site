ad74269b86cabe8de4309f9e052ce6d7628b9f89
Nick Charlton <hello@nickcharlton.net>
2012-09-22 15:18:34 +0100

    Integrates the new template with Hakyll.
    * Provides templates for: Home, atom.xml, Posts, Pages, Tags and Archives.
    * Provides the styles.
    * Sets most of the Hakyll configuration file, site.hs.

---
+++ site.hs
@@ -0,0 +1,120 @@
+{-# LANGUAGE OverloadedStrings, Arrows #-}
+module Main where
+
+import Prelude hiding (id)
+import Control.Category (id)
+import Control.Monad (forM_)
+import Control.Arrow (arr, (>>>), (***), second)
+import Data.Monoid (mempty, mconcat)
+import qualified Data.Map as M
+
+import Hakyll
+
+-- | Entry point
+--
+main :: IO ()
+main = hakyllWith config $ do
+    -- Compile {less}
+    match "css/main.less" $ do
+        route   $ setExtension ".css"
+        -- lessc can't read from stdin         
+        compile $ getResourceString >>> unixFilter "lessc" ["css/main.less", "--yui-compress"]
+
+    -- Compress CSS
+    match "css/*.css" $ do
+        route idRoute
+        compile compressCssCompiler
+
+    -- Render each and every post
+    match "posts/*" $ do
+        route   $ setExtension ".html"
+        compile $ pageCompiler
+            --- store the post contents before we render the template
+            >>> arr (copyBodyToField "description")
+            >>> arr (renderDateField "date" "%B %e, %Y" "Date unknown")
+            >>> renderTagsField "prettytags" (fromCapture "tags/*")
+            >>> applyTemplateCompiler "templates/post.html"
+            --- now it has the template, and we use it for the index
+            >>> arr (copyBodyToField "full")
+            >>> applyTemplateCompiler "templates/default.html"
+            >>> relativizeUrlsCompiler
+
+    -- Archives
+    match "archives.html" $ route idRoute 
+    create "archives.html" $ constA mempty
+        >>> arr (setField "title" "Archives")
+        >>> setFieldPageList recentFirst
+                "templates/post_item.html" "posts" "posts/*"
+        >>> arr applySelf
+        >>> applyTemplateCompiler "templates/posts.html"
+        >>> applyTemplateCompiler "templates/default.html"
+        >>> relativizeUrlsCompiler
+
+    -- Index
+    match "index.html" $ do
+        route idRoute
+        compile $ readPageCompiler
+            >>> arr (setField "title" "Home")
+            >>> requireA "tags" (setFieldA "tags" (renderTagList'))
+            >>> setFieldPageList (take 3 . recentFirst)
+                    "templates/post_full.html" "posts" "posts/*"
+            >>> arr (copyBodyToField "description")
+            >>> arr applySelf
+            >>> applyTemplateCompiler "templates/default.html"
+            >>> relativizeUrlsCompiler
+
+    -- Tags
+    create "tags" $
+        requireAll "posts/*" (\_ ps -> readTags ps :: Tags String)
+
+    -- Add a tag list compiler for every tag
+    match "tags/*" $ route $ setExtension ".html"
+    metaCompile $ require_ "tags"
+        >>> arr tagsMap
+        >>> arr (map (\(t, p) -> (tagIdentifier t, makeTagList t p)))
+
+    -- Read templates
+    match "templates/*" $ compile templateCompiler
+
+    -- Render the 404 page, we don't relativize URL's here.
+    match "404.html" $ do
+        route idRoute
+        compile $ pageCompiler
+            >>> applyTemplateCompiler "templates/default.html"
+
+    -- Render RSS feed
+    match "atom.xml" $ route idRoute
+    create "atom.xml" $ requireAll_ "posts/*" >>> renderAtom feedConfiguration
+
+    -- End
+    return ()
+  where
+    renderTagList' :: Compiler (Tags String) String
+    renderTagList' = renderTagList tagIdentifier
+
+    tagIdentifier :: String -> Identifier (Page String)
+    tagIdentifier = fromCapture "tags/*"
+
+makeTagList :: String
+            -> [Page String]
+            -> Compiler () (Page String)
+makeTagList tag posts =
+    constA posts
+        >>> pageListCompiler recentFirst "templates/post_item.html"
+        >>> arr (copyBodyToField "posts" . fromBody)
+        >>> arr (setField "title" ("Articles tagged with " ++ tag))
+        >>> applyTemplateCompiler "templates/posts.html"
+        >>> applyTemplateCompiler "templates/default.html"
+        >>> relativizeUrlsCompiler
+
+config :: HakyllConfiguration
+config = defaultHakyllConfiguration
+
+feedConfiguration :: FeedConfiguration
+feedConfiguration = FeedConfiguration
+    { feedTitle       = "nickcharlton.net"
+    , feedDescription = "iOS, Robotics, Python, 3D printing, and other thoughts."
+    , feedAuthorName  = "Nick Charlton"
+    , feedAuthorEmail = "hello@nickcharlton.net"
+    , feedRoot        = "http://nickcharlton.net"
+    }
