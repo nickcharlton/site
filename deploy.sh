e2df244dd86f46a04aef82aac5e489d59dce5df7
Nick Charlton <nick@nickcharlton.net>
2015-12-16 22:41:44 +0000

    Adds a basic deploy script.

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/sh
+
+echo "Cleaning and then building..."
+
+bundle exec jekyll clean
+bundle exec jekyll build
+
+echo "Running Rsync..."
+rsync -avzc _site/ nickcharlton.net:/var/www/site
