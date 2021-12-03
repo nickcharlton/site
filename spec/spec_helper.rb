253c579a803ebd3e21c8f99792dcc722e5360728
Nick Charlton <nick@nickcharlton.net>
2021-12-03 16:34:52 +0000

    Add `picture` to simplify using images (#87)
    This makes it easier to insert images into posts, allowing:
    
        {% picture /path/to/image.png %}
        Some description
        {% endpicture %}
    
    This uses a bunch of inspiration from `jekyll-avatar`, which had the
    least worst test setup around. Unfortunately, we still need to generate
    a whole site to test out a tag.
    
    https://jekyllrb.com/docs/plugins/tags/
    https://github.com/jekyll/jekyll-avatar/blob/192986e31b8591d3d9f02e2fe6e9009d620b7a70/spec/spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,46 @@
 require "rspec"
 
 require "jekyll"
+
+TEST_DIR = File.dirname(__FILE__)
+TMP_DIR  = File.expand_path("../tmp", TEST_DIR)
+
+def tmp_dir(*files)
+  File.join(TMP_DIR, *files)
+end
+
+def source_dir(*files)
+  tmp_dir("source", *files)
+end
+
+def dest_dir(*files)
+  tmp_dir("dest", *files)
+end
+
+def build_site(opts = {})
+  defaults = Jekyll::Configuration::DEFAULTS
+  opts = opts.merge(
+    source: source_dir,
+    destination: dest_dir
+  )
+  conf = Jekyll::Utils.deep_merge_hashes(defaults, opts)
+  Jekyll::Site.new(conf)
+end
+
+def collection(site, label = "test")
+  Jekyll::Collection.new(site, label)
+end
+
+def build_doc(opts = {})
+  site = build_site(opts)
+  options = { site: site, collection: collection(site) }
+  doc = Jekyll::Document.new(source_dir("_test/doc.md"), options)
+  doc.merge_data!({ author: "test" })
+  doc
+end
+
+def render(content)
+  doc = build_doc
+  doc.content = content
+  doc.output = Jekyll::Renderer.new(doc.site, doc).run
+end
